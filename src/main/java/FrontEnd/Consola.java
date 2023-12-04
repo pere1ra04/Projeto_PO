70352e0a4661e28d4597dde13b0f5f7ef4cd650d
pere1ra04 <pereira04.jp@gmail.com>
2023-12-04 19:54:46 +0000

    V0.06
    Criação de todos os menus

--- src/main/java/FrontEnd/Consola.java
+++ src/main/java/FrontEnd/Consola.java
@@ -14,111 +14,8 @@ import java.util.Scanner;
  */
 public class Consola {
     private final Scanner scanner = new Scanner(System.in);
-
-    public void login(){
-            converterParaAscii("Login:",100);
-            escrever("Username:");
-            String nome = scanner.nextLine();
-            //verificaçoes
-            escrever("Password:");
-            String password = scanner.nextLine();
-            //verificaçoes
-            //depois das verficaçoes chamar sistema.exibirMenuPrincipal();
-        }
     
-        public void exibirMenuPrincipal() {
-            while (true) {
-                converterParaAscii("Gestao de Departamento",100);
-                escrever("\n----- Menu Principal -----");
-                escrever("1. Entrar como Administrador");
-                escrever("2. Entrar como Professor");
-                escrever("3. Sair");
-
-            System.out.print("Escolha uma opção: ");
-            int opcao = scanner.nextInt();
-            switch (opcao) {
-                case 1:
-                    menuAdministrador();
-                    break;
-                case 2:
-                    menuProfessores();
-                    break;
-                case 3:
-                    escrever("Saindo do programa. Até mais!");
-                    scanner.close();
-                    System.exit(0);
-                    break;
-                default:
-                    escreverErro("Opção inválida. Tente novamente.");
-            }
-            }
-    }
-    
-    public void menuAdministrador(){
-    converterParaAscii("Menu Admistrador",20);
-    escrever ("1-Informações de Professores");
-    escrever ("2-Informações de Cursos");
-    escrever ("3-Informações de UCS");
-    escrever ("0-Sair");
-    
-    escrever ("Inttroduza a opção pretendida");
-    int opcao = scanner.nextInt();
-    switch(opcao){
-        case 1:
-        //info
-        break;
-        
-        case 2:
-        //info
-        break;
-        
-        case 3:
-        //info
-        break;
-        
-        case 0:
-        //info
-        break;
-        
-        default:
-        escreverErro("Opção inválida. Tente novamente.");
-    }
-    }
-    
-    public void menuProfessores(){
-        //fazer verficações para saber quais as opcoes disponivies consuante o seu cargo
-    converterParaAscii("Menu Professores",20);
-    escrever ("1-");
-    escrever ("2-");
-    escrever ("3-");
-    escrever ("0-");
-    
-    escrever ("Inttroduza a opção pretendida");
-    int opcao = scanner.nextInt();
-    switch(opcao){
-        case 1:
-        //info
-        break;
-        
-        case 2:
-        //info
-        break;
-        
-        case 3:
-        //info
-        break;
-        
-        case 0:
-        //info
-        break;
-        
-        default:
-        escreverErro("Opção inválida. Tente novamente.");
-    }
-    }
-    
-    
-    
+  
     public void escrever(String mensagem) {
         System.out.println(mensagem);
     }
@@ -132,7 +29,34 @@ public class Consola {
         return scanner.nextLine();
     }
     
-    private static void converterParaAscii(String texto, int tamanhoFonte){
+    public int lerInteiro(String[] opcoes) {
+        Integer numero = null;
+        String texto = "";
+
+        do {
+            escrever("Selecione uma das seguintes opcões:");
+            for (int i = 0; i < opcoes.length; i++) {
+                escrever((i + 1) + " - " + opcoes[i]);
+            }
+
+            try {
+                texto = scanner.nextLine();
+                numero = Integer.valueOf(texto);
+            } catch (NumberFormatException e) {
+                escreverErro(texto + " não é uma opção válida");
+            }
+
+            if (numero == null || numero <= 0 || numero > opcoes.length) {
+                numero = null;
+                escreverErro(texto + " não é uma opção válida");
+            }
+
+        } while (numero == null);
+
+        return numero;
+    }
+    
+    public static void converterParaAscii(String texto, int tamanhoFonte){
         Font fonte = new Font("Lato", Font.PLAIN, tamanhoFonte);
         for (int i = 0; i < texto.length(); i++) {
             char caractere = texto.charAt(i);
@@ -147,5 +71,5 @@ public class Consola {
             }
         }
     }
-  
+
 }
--- src/main/java/FrontEnd/ProjectPO.java
+++ src/main/java/FrontEnd/ProjectPO.java
@@ -13,12 +13,215 @@ import java.awt.Font;
  *
  * @author user
  */
-public class ProjectPO extends Consola {
+public class ProjectPO{
+   
     private final Consola consola = new Consola();
     private final Sistema sistema = new Sistema();
     
-     public static void main(String[] args) {
-        ProjectPO sistema = new ProjectPO();
-        sistema.login();
+     public static void main(String[] args) throws Exception{
+     ProjectPO programa = new ProjectPO();
+     programa.login();
+     }
+
+    public void login(){
+        
+            consola.converterParaAscii("Login:",100);
+            
+            String numero = consola.lerString("Numero:");
+            //verificaçoes
+            String password = consola.lerString("Password");
+            //verificaçoes
+            exibirMenuPrincipal();
+        }
+    
+        public void exibirMenuPrincipal() {
+            while (true) {
+                consola.converterParaAscii("Gestao de Departamento",100);
+                consola.escrever("\n----- Menu Principal -----");
+                
+                int opcao;
+                String[] opcoes = {
+                    "Entrar como Administrador",
+                    "Entrar como Professor",
+                    "Entrar como Regente de UC",
+                    "Entrar como Diretor de Curso",
+                    "Sair",
+                };
+                
+
+            consola.escrever("Escolha uma opção: ");
+            opcao = consola.lerInteiro(opcoes);
+            switch (opcao) {
+                case 1:
+                    menuAdministrador();
+                    break;
+                case 2:
+                    menuProfessores();
+                    break;
+                case 3:
+                    menuRegente();
+                    break;
+                case 4:
+                    menuDiretorCurso();
+                    break;
+                case 5:
+                    consola.escrever("Saindo do programa. Até mais!");
+                    System.exit(0);
+                    break;
+                default:
+                    consola.escreverErro("Opção inválida. Tente novamente.");
+            }
+            }
     }
+    
+    public void menuAdministrador(){
+    
+    consola.converterParaAscii("Menu Admistrador",20);
+    int opcao;
+    String[] opcoes = {
+        "Informações de Professores",
+        "Informações de Cursos",
+        "Informações de UCS",
+        "Sair",
+    };
+    
+    
+    consola.escrever ("Inttroduza a opção pretendida");
+    
+    opcao = consola.lerInteiro(opcoes);
+    switch(opcao){
+        case 1:
+        //info
+        break;
+        
+        case 2:
+        //info
+        break;
+        
+        case 3:
+        //info
+        break;
+        
+        case 4:
+        //info
+        break;
+        
+        default:
+        consola.escreverErro("Opção inválida. Tente novamente.");
+    }
+    }
+    
+    public void menuProfessores(){
+    
+    consola.converterParaAscii("Menu Professor",20);
+    int opcao;
+    String[] opcoes = {
+        "Criar Sumario",
+        "Consultar Sumarios",
+        "Consultar Serviço Docente",
+        "Sair",
+    };
+    
+    
+    consola.escrever ("Inttroduza a opção pretendida");
+    
+    opcao = consola.lerInteiro(opcoes);
+    switch(opcao){
+        case 1:
+        //info
+        break;
+        
+        case 2:
+        //info
+        break;
+        
+        case 3:
+        //info
+        break;
+        
+        case 4:
+        //info
+        break;
+        
+        default:
+        consola.escreverErro("Opção inválida. Tente novamente.");
+    }
+    }
+    
+    
+    public void menuRegente(){
+    
+    consola.converterParaAscii("Menu Regente da UC: ",20); //ADICIONAR O NOME DA UC POR REFERNCIA
+    int opcao;
+    String[] opcoes = {
+        "Adicionar Aluno",
+        "Remover Aluno",
+        "Consultar Assiduidade",
+        "Sair",
+    };
+    
+    
+    consola.escrever ("Inttroduza a opção pretendida");
+    
+    opcao = consola.lerInteiro(opcoes);
+    switch(opcao){
+        case 1:
+        //info
+        break;
+        
+        case 2:
+        //info
+        break;
+        
+        case 3:
+        //info
+        break;
+        
+        case 0:
+        //info
+        break;
+        
+        default:
+        consola.escreverErro("Opção inválida. Tente novamente.");
+    }
+    }
+    
+    public void menuDiretorCurso(){
+    
+    consola.converterParaAscii("Menu Diretor de Curso    : ",20); //ADICIONAR O NOME Do curso POR REFERNCIA
+    int opcao;
+    String[] opcoes = {
+        "Alterar Designação do Curso",
+        "Listar Alunos",
+        "Listar Professores",
+        "Sair",
+    };
+    
+    
+    consola.escrever ("Inttroduza a opção pretendida");
+    
+    opcao = consola.lerInteiro(opcoes);
+    
+    switch(opcao){
+        case 1:
+        //info
+        break;
+        
+        case 2:
+        //info
+        break;
+        
+        case 3:
+        //info
+        break;
+        
+        case 4:
+        //info
+        break;
+        
+        default:
+        consola.escreverErro("Opção inválida. Tente novamente.");
+    }
+    }   
+
 }
